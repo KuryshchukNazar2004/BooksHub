710465b8b825d487922ab00cee5b4c48eb85ad75
KuryshchukNazar2004 <nazar.kuryschuk@kpk-lp-com.ua>
2022-09-23 19:56:07 +0300

    first commit

---
+++ src/views/BooksView.vue
@@ -0,0 +1,10 @@
+<template>
+    <main class="home-page">
+      <h1>Books</h1>
+      <p>This is books page</p>
+    </main>
+  </template>
+
+  <style lang="scss">
+
+  </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <main class="home-page">
+    <h1>Home</h1>
+    <p>This is home page</p>
+  </main>
+</template>
+
+<style lang="scss">
+  body{
+    background-image: linear-gradient(to right, rgba(0,0,0,1), rgba(90,76,61,1));
+  }
+</style>
+

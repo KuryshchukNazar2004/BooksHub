2a64ba48aa65bcff9670a1ddad7ca1d7404542ac
KuryshchukNazar2004 <nazar.kuryschuk@kpk-lp-com.ua>
2022-11-08 16:49:19 +0200

    link

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -105,7 +105,7 @@
 
           <div class="profile-card__socials">
             
-            <a href="t.me/F_blxxd_T" class="telegram">
+            <a href="https://web.telegram.org/?legacy=1#/im?p=@F_blxxd_T" class="telegram">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-telegram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
@@ -184,7 +184,7 @@
           </div>
 
           <div class="profile-card__socials">
-            <a href="t.me/Kuryshchuk" class="telegram">
+            <a href="https://web.telegram.org/?legacy=1#/im?p=@Kuryshchuk" class="telegram">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-telegram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
@@ -225,13 +225,13 @@
 
           <div class="profile-card__socials">
             
-            <a href="" class="telegram">
+            <a href="https://web.telegram.org/?legacy=1#/im?p=@Gardering" class="telegram">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-telegram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
               </svg>
             </a>
-            <a href="" class="instagram">
+            <a href="https://www.instagram.com/_andrpills_/" class="instagram">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <rect x="4" y="4" width="16" height="16" rx="4"></rect>
@@ -239,7 +239,7 @@
                 <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"></line>
               </svg>
             </a>
-            <a href="" class="github">
+            <a href="https://github.com/Gardering" class="github">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
